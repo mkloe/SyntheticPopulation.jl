9b4dc8b9a37f2303267758d59f5392209e7eac1e
Marcin Zurek <mz.mzurek@gmail.com>
2023-04-25 22:37:41 +0200

    simplify merge_distributions, dataframe_preparatio

--- src/merge_distributions.jl
+++ src/merge_distributions.jl
@@ -2,61 +2,6 @@
 #Transportation Research Record, 2014(1), 92-101.
 
 
-function get_no_ipf_df(no_ipf_merged_attributes::DataFrame, ipf_df1::DataFrame, ipf_df2::DataFrame)
-    columns = names(no_ipf_merged_attributes)
-    deleteat!(columns, findall(in(names(ipf_df2)), columns))
-    no_of_unique_values = map(x -> length(unique(no_ipf_merged_attributes[:,x])), columns)
-    column_index = only(findall(!=(1), no_of_unique_values))
-    marginal_attr_values = combine(groupby(ipf_df1, [columns[column_index]]), :population => sum)
-    rename!(marginal_attr_values, :population_sum => :population)
-    
-    return marginal_attr_values
-end
-
-
-function calculate_no_ipf_joint_distribution(dfs_for_ipf::Dict{String, DataFrame})
-    no_ipf_merged_attributes = copy(dfs_for_ipf["no_ipf_merged_attributes"])
-    no_ipf_df1 = dfs_for_ipf["no_ipf_df1"]
-    no_ipf_df2 = dfs_for_ipf["no_ipf_df2"]
-    no_ipf_joint_distribution = copy(no_ipf_merged_attributes)
-
-    #size of target popoulation_size
-    no_ipf_df1_nrow = nrow(no_ipf_df1)
-    no_ipf_df2_nrow = nrow(no_ipf_df2)
-
-    #add population size for no_ipf_joint_distribution
-    if no_ipf_df1_nrow == 0 && no_ipf_df2_nrow == nrow(no_ipf_joint_distribution)
-        sort!(no_ipf_df2, Not(:population))
-        colnames = names(no_ipf_df2)
-        sort!(no_ipf_joint_distribution, deleteat!(colnames, length(colnames)))
-        no_ipf_joint_distribution.:population = no_ipf_df2.:population
-    elseif no_ipf_df2_nrow == 0 && no_ipf_df1_nrow == nrow(no_ipf_joint_distribution)
-        sort!(no_ipf_df1, Not(:population))
-        colnames = names(no_ipf_df1)
-        sort!(no_ipf_joint_distribution, deleteat!(colnames, length(colnames)))
-        no_ipf_joint_distribution.:population = no_ipf_df1.:population
-    else 
-        if no_ipf_df1_nrow * no_ipf_df2_nrow == nrow(no_ipf_joint_distribution)
-            nothing
-        elseif no_ipf_df1_nrow == 0
-            no_ipf_df1 = get_no_ipf_df(no_ipf_merged_attributes, ipf_df1, ipf_df2)
-        elseif no_ipf_df2_nrow == 0
-            no_ipf_df2 = get_no_ipf_df(no_ipf_merged_attributes, ipf_df2, ipf_df1)
-        end
-        
-        no_ipf_merged_attributes.:compute_ipf = Int.(ones(nrow(no_ipf_merged_attributes)))
-        no_ipf_dfs_for_ipf = Dict(
-        "ipf_df1" => no_ipf_df1,
-        "ipf_df2" => no_ipf_df2,
-        "ipf_merged_attributes" => no_ipf_merged_attributes
-        )
-        no_ipf_joint_distribution = fit_ipf(no_ipf_dfs_for_ipf)
-    end
-    
-    return no_ipf_joint_distribution
-end
-
-
 function fit_ipf(dfs_for_ipf::Dict{String, DataFrame})
     ipf_merged_attributes = dfs_for_ipf["ipf_merged_attributes"]
     ipf_df1 = dfs_for_ipf["ipf_df1"]
@@ -79,31 +24,18 @@ function fit_ipf(dfs_for_ipf::Dict{String, DataFrame})
         if isnan(Array(af)[1]) #error handling
             input_sample = input_sample .+ 0.00000000000000000001 # to avoid dividing by zero
             af = ipf(input_sample, input_marginals)
-            X_prop = input_sample ./ sum(input_sample)
-            Z = X_prop .* Array(af)
-            population_size = min(sum(ipf_df1.:population), sum(ipf_df2.:population))
-            Z = population_size .* Z
-            Z = Int.(round.(Z))
-        else
-            X_prop = input_sample ./ sum(input_sample)
-            Z = X_prop .* Array(af)
-            population_size = min(sum(ipf_df1.:population), sum(ipf_df2.:population))
-            Z = population_size .* Z
-            Z = Int.(round.(Z))
         end
+        X_prop = input_sample ./ sum(input_sample)
+        Z = X_prop .* Array(af)
+        population_size = max(sum(ipf_df1.:population), sum(ipf_df2.:population))
+        Z = population_size .* Z
+        Z = Int.(round.(Z))
     end
     
     #generate output df from ipf
     joint_distribution = copy(ipf_merged_attributes)
     joint_distribution.:population = vec(Z)
     joint_distribution = joint_distribution[:, Not(:compute_ipf)]
-    
-    #generate output df not in ipf
-    if haskey(dfs_for_ipf, "no_ipf_merged_attributes")
-        no_ipf_joint_distribution = calculate_no_ipf_joint_distribution(dfs_for_ipf)  
-        allowmissing!(joint_distribution)
-        append!(joint_distribution, no_ipf_joint_distribution)
-    end
     
     return joint_distribution
 end
@@ -122,20 +54,6 @@ function get_dfs_for_ipf_slice(dfs_for_ipf::Dict{String, DataFrame}, unique_valu
     dfs_for_ipf_slice["ipf_df1"] = ipf_df1_slice
     dfs_for_ipf_slice["ipf_df2"] = ipf_df2_slice
     dfs_for_ipf_slice["ipf_merged_attributes"] = ipf_merged_attributes_slice
-
-    if haskey(dfs_for_ipf, "no_ipf_merged_attributes")
-        no_ipf_merged_attributes = copy(dfs_for_ipf["no_ipf_merged_attributes"])
-        no_ipf_df1 = copy(dfs_for_ipf["no_ipf_df1"])
-        no_ipf_df2 = copy(dfs_for_ipf["no_ipf_df2"])
-
-        no_ipf_df1_slice = filter(column => n -> n == unique_value, no_ipf_df1)
-        no_ipf_df2_slice = filter(column => n -> n == unique_value, no_ipf_df2)
-        no_ipf_merged_attributes_slice = filter(column => n -> n == unique_value, no_ipf_merged_attributes)
-
-        dfs_for_ipf_slice["no_ipf_df1"] = no_ipf_df1_slice
-        dfs_for_ipf_slice["no_ipf_df2"] = no_ipf_df2_slice
-        dfs_for_ipf_slice["no_ipf_merged_attributes"] = no_ipf_merged_attributes_slice
-    end
 
     return dfs_for_ipf_slice
 end 
@@ -174,7 +92,43 @@ function compute_joint_distributions(dfs_for_ipf::Dict{String, DataFrame}; share
 end
 
 
+function apply_missing_config(joint_distribution::DataFrame, missing_config::JSON3.Array{JSON3.Object, Vector{UInt8}, SubArray{UInt64, 1, Vector{UInt64}, Tuple{UnitRange{Int64}}, true}})
+    attribute_names = names(joint_distribution)    
+    for dictionary in missing_config
+        if_column, if_values, then_column, then_values = get_config_elements(dictionary)
+        
+        if String(if_column) in attribute_names && String(then_column) in attribute_names
+            temp_df = copy(joint_distribution)
+            allowmissing!(joint_distribution)
+            
+            #find indices for "if"s
+            if typeof(if_values[1]) == String
+                temp_df = filter(Symbol(if_column) => x -> in(if_values, x), temp_df)
+            elseif typeof(if_values[1]) == Int
+                temp_df = filter(Symbol(if_column) => x -> x in if_values, temp_df)
+            end
+            joint_distribution[temp_df.:id, then_column] .= missing
+        end
+    end
+
+    #split-apply-combine procedure for the columns including missing
+    deleteat!(attribute_names, findall(x -> String(x) in ["population", "id"], attribute_names))
+    aggregated_joint_distribution = groupby(joint_distribution, attribute_names)
+    aggregated_joint_distribution = combine(aggregated_joint_distribution, :population => sum)
+    
+    #return proper dataframes
+    aggregated_joint_distribution.:population = convert.(Int, aggregated_joint_distribution[:, :population_sum])
+    aggregated_joint_distribution = aggregated_joint_distribution[:, Not(:population_sum)]
+
+    return aggregated_joint_distribution
+end
+
+
 function generate_joint_distributions(marginal_attributes::DataFrame ...; config_file::Union{Nothing, String} = nothing)
+
+    for dataframe in marginal_attributes
+        sort!(dataframe, reverse(deleteat!(names(dataframe), findall(x -> String(x) == "population", names(dataframe)))))
+    end
 
     if length(marginal_attributes) == 1
         joint_distribution = marginal_attributes[1]
@@ -185,19 +139,25 @@ function generate_joint_distributions(marginal_attributes::DataFrame ...; config
         for i in 2:(length(marginal_attributes))
             dfs_for_ipf = merge_attributes(joint_distribution, marginal_attributes[i]; config_file = config_file)
             joint_distribution = compute_joint_distributions(dfs_for_ipf)
+            sort!(joint_distribution, reverse(deleteat!(names(joint_distribution), findall(x -> String(x) == "population", names(joint_distribution)))))
         end
     end
-
-    #return proper dataframes
+    
+    #add ID column
     joint_distribution.:id = collect(1:nrow(joint_distribution))
-    aggregated_joint_distribution = joint_distribution[:, [:id, :population]]
-    aggregated_joint_distribution.:population = convert.(Int, aggregated_joint_distribution[:, :population])
-    joint_distribution = joint_distribution[:, Not(:population)]
-    joint_distribution = joint_distribution[:, unique(vcat("id", names(joint_distribution)))] #change order
 
-
-    return joint_distribution, aggregated_joint_distribution
+    #set the attribute values from MISSING config file to missing
+    if config_file !== nothing
+        config_file = read_json_file(config_file)
+        missing_config = config_file["missing_config"]
+        if missing_config != "missing"
+            aggregated_joint_distribution = apply_missing_config(joint_distribution, missing_config)
+        else
+            aggregated_joint_distribution = joint_distribution
+        end
+    else 
+        aggregated_joint_distribution = joint_distribution
+    end
+    
+    return aggregated_joint_distribution
 end
-
-##TO DO:
-#- sort columns (from last to first) for input dataframes to ensure correct merging of data
